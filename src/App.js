aca85ecbab8bc89d5b05d673e8df0e67a115cbe9
nwardle <nwardle@gmail.com>
2021-12-15 20:57:40 +0000

    Lesson 50

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import { Component } from 'react';
 import { CardList } from './components/card-list/card-list-component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     }
   }
 
@@ -18,11 +20,24 @@ class App extends Component {
       .then(data => this.setState({ monsters: data }))
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render () {
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
-
+        <SearchBox 
+          placeholder={'Search Cats'}
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
