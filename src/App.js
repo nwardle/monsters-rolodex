73370f6900cd962f2f0180558f944da50c525615
nwardle <nwardle@gmail.com>
2021-12-15 18:50:01 +0000

    Just Starting

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 
-
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import { CardList } from './components/card-list/card-list-component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        { name: 'Frankenstein', id: 'asc0' },
-        { name: 'Dracula', id: 'asc1'  },
-        { name: 'Zombie',  id: 'asc2' },
-      ]
+      monsters: []
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => this.setState({ monsters: data }))
   }
 
   render () {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))
-        }
+        <CardList monsters={this.state.monsters} />
+
       </div>
     );
   }
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,6 @@
+
+export const Card = props => (
+  <div>
+    <h1>{props.monster.name}</h1>
+  </div>
+)
