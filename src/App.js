948c25b88a42ce04a582f8612ea3971e1ab7ec53
nwardle <nwardle@gmail.com>
2021-12-16 19:01:10 +0000

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Cat Rolodex</h1>
         <SearchBox 
           placeholder={'Search Cats'}
           handleChange={this.handleChange}
